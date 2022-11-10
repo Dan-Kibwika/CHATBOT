4f781da305bccba97c54c3d8b412ec87f91d6511
Dan-kibwika <83333064+Dan-Kibwika@users.noreply.github.com>
2022-11-10 20:15:33 +0200

    Add files via upload

---
+++ chatBot/DtaLab.py
@@ -0,0 +1,14 @@
+
+import csv
+import pandas as pd
+
+%matplotlib inline
+import seaborn as sns
+import warnings
+warnings.filtermarnings('ignore')
+
+
+r = requests.get('')
+iris_data = [str.split]
+
+
---
+++ chatBot/training.py
@@ -0,0 +1,29 @@
+import json
+import pickle
+import random
+import nltk
+import numpy as np
+from nltk.stem import wordnetLematizer
+import tensorflow as tf
+from tensorflow import keras
+from keras import layers
+from keras.models import Sequential
+from keras.layers import Dense, Activation, Dropout
+
+lematizer = wordnetLematizer
+intents = json.loads(open('intents.json').read())
+
+words = []
+classes = []
+documents = []
+ignore_letters = ['?', '!', ',', ';', '<', '>', '=']
+
+for intent in intents['intents']:
+    for pattern in intents['patterns']:
+        word_list = nltk.word_tokenize(pattern)
+        words.append(word_list)
+        documents.append((word_list, intents['tag']))
+        if intent['tag'] not in classes:
+            classes.append(intent['tag'])
+
+        print(documents)
